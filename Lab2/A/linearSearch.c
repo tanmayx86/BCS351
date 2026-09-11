b3d90dc93ed532323c52bb373ae0a59d725efa96
tanmayx86 <tanmay.25gcebds091@galgotiacollege.edu>
2026-09-11 05:09:02 +0000

    linear

--- Lab2/A/linearSearch.c
+++ Lab2/A/linearSearch.c
@@ -4,12 +4,6 @@
 int linearSearch(int crr[], int n, int key)
  { 
 
-    //  //Printing the addresses od Array crr
-    // for (int i = 0; i < n; i++) 
-    // { 
-    //      printf("The address of Array arr at index %d is : %p \n", i, &crr[i]);
-    // } 
-
     for (int i = 0; i < n; i++) 
     { 
         if (crr[i] == key) 
@@ -23,18 +17,12 @@ int main()
 {
     int arr[] = {5, 12, 7, 25, 18}; 
     int n = sizeof(arr) / sizeof(arr[0]); 
-     //Printing the addresses od Array arr
-    // for (int i = 0; i < n; i++) 
-    // { 
-    //      printf("The address of Array arr at index %d is : %p \n", i, &arr[i]);
-    // } 
-
+    
 
     int key = 25; 
     printf("Enter the value of key to be searched: \t");
     scanf("%i", &key );
-    // printf("The address of key is : %p \n", &key);
-
+   
     //Calling linearSearch()
     int result = linearSearch(arr, n, key);   //? Pass by value ? or by Reference?
 
